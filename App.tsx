6fafeb6d5ba19b41f0eb32dae7ef0f2771f5392e
srishakthim <srishakthimoorthy235@gmail.com>
2025-12-09 13:13:53 +0530

    screens are completed

--- App.tsx
+++ App.tsx
@@ -1,12 +1,3 @@
-// import PatientDashboard from "./src/screens/PatientDashboard";
-
-// const App = () => {
-//     return <PatientDashboard />;
-// }
-
-// export default App;
-
-
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
